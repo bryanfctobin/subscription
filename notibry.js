80dd8b6a9d0007511d31a1c2d5f4e357145d2095
Bryan Tobin <bryanfctobin@gmail.com>
2021-02-12 09:18:09 -0500

    Set default display of notification link to none. Adding logic to notibry.js to display the Notification link only if permission is default

--- notibry.js
+++ notibry.js
@@ -1,8 +1,14 @@
+//Ref: https://developer.mozilla.org/en-US/docs/Web/API/notification
 document.addEventListener("DOMContentLoaded", setupNotifications);
 function setupNotifications() {
   let nbtn = document.querySelector("#notificationsBtn");
-  nbtn.addEventListener("click", function () {
-    window.Notification.requestPermission();
-    console.log(window.Notification.permission);
-  });
+  if (!("Notification" in window)) {
+    console.log("This browser does not support desktop notifications");
+  } else if (Notification.permission === "default") {
+    nbtn.style.display = "inline-block";
+    nbtn.addEventListener("click", function () {
+      window.Notification.requestPermission();
+      console.log(window.Notification.permission);
+    });
+  }
 }
